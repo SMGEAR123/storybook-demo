d322c1fac4fd7d32df8017e78985cf7ea931638c
“Sam <smans123@gmail.com>
2019-11-18 19:42:50 -0500

    Fixing file types for story files

--- src/stories/component-a.stories.ts
+++ src/stories/component-a.stories.ts
@@ -2,7 +2,7 @@ import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 
 import { ComponentAComponent } from '../app/components/component-a/component-a.component';
-import * as markdown from './notes/a.notes.md';
+// import * as markdown from './notes/a.notes.md';
 
 
 storiesOf('Component A', module)
@@ -12,19 +12,19 @@ storiesOf('Component A', module)
             name: 'Chris',
             myEvent: action('Hello Chris!')
         }
-    }), { notes: {markdown} })
+    }))
     .add('Jane', () => ({
         component: ComponentAComponent,
         props: {
           name: 'Jane',
           myEvent: action('Hello Jane!')
         },
-      }), { notes: {markdown} })
+      }))
       .add('Joe', () => ({
         component: ComponentAComponent,
         props: {
           name: 'Joe',
           myEvent: action('Hello Joe!')
         },
-      }), { notes: {markdown} });
+      }));
 
